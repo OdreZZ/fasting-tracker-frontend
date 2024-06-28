19a1d1d4eae7410d4d074b6c386b01a08c1f8a36
ZZerDo <zerdo.zaid+git@gmail.com>
2024-06-28 05:12:03 +0200

    Added a fix for Safari

--- pages/_document.js
+++ pages/_document.js
@@ -4,10 +4,23 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
-                <link rel="manifest" href="/manifest.webmanifest" />
-                <script async src="https://unpkg.com/pwacompat" crossOrigin="anonymous"></script>
-                <link rel="apple-touch-icon" href="/icons/icon.png"></link>
                 <meta name="theme-color" content="#3D8361" />
+                <link rel="apple-touch-icon" href="/icons/icon.png"></link>
+                <link rel="manifest" href="/manifest.webmanifest" />
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.pwacompat = {
+                                manifest: '/manifest.webmanifest',
+                                background_color: '#3D8361'
+                            };
+                            if (('standalone' in navigator) && (!navigator.standalone)) {
+                                import('https://unpkg.com/pwacompat');
+                            }
+                        `,
+                    }}
+                />
+                <script async src="https://unpkg.com/pwacompat" crossOrigin="anonymous"></script>
             </Head>
             <body>
                 <Main />
