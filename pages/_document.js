ecece6da7bf0eac580b4cba44ffd30e402451474
ZZerDo <zerdo.zaid+git@gmail.com>
2024-06-28 04:52:43 +0200

    Updated theme color in document

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
                 <link rel="manifest" href="/manifest.webmanifest" />
                 <script async src="https://unpkg.com/pwacompat" crossOrigin="anonymous"></script>
                 <link rel="apple-touch-icon" href="/icons/icon.png"></link>
-                <meta name="theme-color" content="#FFFFFF" />
+                <meta name="theme-color" content="#1C6758" />
             </Head>
             <body>
                 <Main />
