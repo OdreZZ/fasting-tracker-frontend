7f82f6c2ab814db4d863ba0eea77c02f3e90e0d3
OdreZZ <zerdo.zaid+git@gmail.com>
2025-02-27 23:48:36 +0100

    Updated theme color

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
-                <meta name="theme-color" content="#3D8361" />
+                <meta name="theme-color" content="#0F52BA" />
                 <link rel="apple-touch-icon" href="/icons/icon.png"></link>
                 <link rel="manifest" href="/manifest.webmanifest" />
                 <script
@@ -12,7 +12,7 @@ export default function Document() {
                         __html: `
                             window.pwacompat = {
                                 manifest: '/manifest.webmanifest',
-                                background_color: '#3D8361'
+                                background_color: '#0F52BA'
                             };
                             if (('standalone' in navigator) && (!navigator.standalone)) {
                                 import('https://unpkg.com/pwacompat');
